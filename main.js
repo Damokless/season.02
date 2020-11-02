b01b87301694c1953cdec8d00fd5b494df07cc0e
damokless <damokless@protonmail.com>
2020-11-02 15:44:43 +0100

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-const eventbox = require ('./eventbox.js')
+/*const eventbox = require ('./eventbox.js')
 const names = ['Luffy', 'Zoro', 'Usopp', 'Robin', 'Nami', 'Sanji', 'Chopper']
-eventbox.withArgs(names)
+eventbox.withArgs(names)*/
+
+const streambox = require ('./streambox.js')
+
+streambox.duplicate()
